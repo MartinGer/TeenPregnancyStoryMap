1609d5cd8c13b9a65e52390448131b19f08e96f8
MartinGer <Gerstmaier.Martin@gmx.net>
2018-02-15 19:47:34 +0100

    fix background

--- src/slides/ReligionUSStates/index.js
+++ src/slides/ReligionUSStates/index.js
@@ -10,18 +10,30 @@ export default class ReligionUSStates extends Component {
   render() {
     return (
       <div>
-       <div class="container">
-                    <div class="section no-pad-bot">
-                        <div class="container">
-                            <h2 class="header center">Religion</h2>
-                        </div>
-                    </div>
-                    <div class="section lower">
-                        <p class="text">In average, Americans seem to be more religious than Europeans, whereby we can see variations between the different states as well. Those religions opinions and their supporters way of living them out are so diversified though, that we shouldn’t expect correlations between pure adherent numbers and the teen pregnancy rates.
+      <div class="container">
+          <div class="section no-pad-bot">
+              <h2 class="header center">Religion</h2>
+          </div>
+              <div class="section col s4">
+              <div class="card">
+                  <div class="card-image waves-effect waves-block waves-light">
+                  </div>
+                  <div class="card-content">
+                      <h5>
+                      In average, Americans seem to be more religious than Europeans, whereby we can see variations between the different states as well. Those religions opinions and their supporters way of living them out are so diversified though, that we shouldn’t expect correlations between pure adherent numbers and the teen pregnancy rates.
                                         We have to dig deeper:
-                        </p>
-                    </div>
-                </div>
+                      </h5>
+                  </div>
+                  <div class="card-action">
+                      <a>What happend in 2007?</a>
+                  </div>
+                  <div class="card-reveal">
+                      <span class="card-title grey-text text-darken-4">Abstinence-Only vs Comprehensive
+                          <i class="material-icons right">close</i></span>
+                  </div>
+                  </div>
+          </div>
+          </div>
       </div>
     );
   }
