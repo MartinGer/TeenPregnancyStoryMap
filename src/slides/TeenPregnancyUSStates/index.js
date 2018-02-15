82b3f703c9a8cb88b63c937050c5e73127b4ad90
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-02-15 20:29:42 +0100

    Add material slider

--- src/slides/TeenPregnancyUSStates/index.js
+++ src/slides/TeenPregnancyUSStates/index.js
@@ -2,6 +2,8 @@ import React, {Component, View} from 'react';
 import {Chart} from 'react-google-charts';
 import './index.css';
 import {getStateName} from "./fips";
+import {Slider} from "material-ui";
+import _ from 'underscore';
 
 export default class TeenPregnancyUSStates extends Component {
     constructor(props) {
@@ -13,16 +15,15 @@ export default class TeenPregnancyUSStates extends Component {
             colorAxis: {colors: ['#edefff', '#303F9F']}
         };
 
-        this.state = {options: options, data: [['State', 'Teenage Births Per 1000 Teens']]};
+        this.state = {year: 2003, options: options, data: [['State', 'Teenage Births Per 1000 Teens']]};
     }
 
     componentWillMount() {
-        this.fetchData(2003);
+        this.fetchData();
     }
 
-    fetchData(year) {
-        console.log('Fetch');
-        fetch('http://vm-tuk2-team03.eaalab.hpi.uni-potsdam.de/api/usa/?year=' + year)
+    fetchData() {
+        fetch('http://vm-tuk2-team03.eaalab.hpi.uni-potsdam.de/api/usa/?year=' + this.state.year)
             .then(results => {
                 console.log(results);
                 return results.json()
@@ -44,18 +45,26 @@ export default class TeenPregnancyUSStates extends Component {
         )
     }
 
+    handleSlider = (event, year) => {
+        this.setState({year: year});
+        this.fetchData();
+    };
 
     render() {
         return (
-            <div id="teenMapUsa" class="container center">
-                <Chart
-                    chartType="GeoChart"
-                    data={this.state.data}
-                    options={this.state.options}
-                    graph_id="GeoChart"
-                    legend_toggle={true}
-                    width={"1000px"}
-                    height={"600px"}/>
+            <div class="container center">
+                <h4>US Teen Pregnancy in {this.state.year}</h4>
+                <div class="container center">
+                    <Chart
+                        chartType="GeoChart"
+                        data={this.state.data}
+                        options={this.state.options}
+                        graph_id="GeoChart"
+                        legend_toggle={true}
+                        width={"1000px"}
+                        height={"600px"}/>
+                </div>
+                <Slider step={1} value={this.state.year} min={2003} max={2016} onChange={_.debounce(this.handleSlider, 200)}/>
             </div>
         );
     }
