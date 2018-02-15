66c9095cd78bb6eea08a3a33ac8297d78fde3dfc
Sven Lehmann <sven.lehmann93@gmail.com>
2018-02-15 10:01:27 +0100

    Small fix for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'public/');
-var APP_DIR = path.resolve(__dirname, 'src/');
+var BUILD_DIR = path.resolve(__dirname, './public/');
+var APP_DIR = path.resolve(__dirname, './src/');
 
 var config = {
   entry: APP_DIR + '/index.js',
@@ -26,7 +26,11 @@ var config = {
         use: [ 'style-loader', 'css-loader' ]
       }
     ]
-  }
+  },
+  devServer: {
+    contentBase: "./public",
+    hot: true
+},
 };
 
 module.exports = config;
