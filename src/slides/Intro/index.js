938536173517ac1bd575411dd21d0be084ba781d
DuyHung Nguyen <duyhung.nguyen@student.hpi.de>
2018-02-15 19:06:33 +0100

    1st slide

--- src/slides/Intro/index.js
+++ src/slides/Intro/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './index.css'
 
 export default class Intro extends Component {
   constructor(props) {
@@ -8,46 +9,138 @@ export default class Intro extends Component {
   }
 
   render() {
-    return (
-      <div>
-        Teenage Pregnancy in the USA and factors influencing its occurence
-        <div>
-        <div>
-          We want to have a look on pregnant teenage girls of the age of 15 to 19 whereby those normally haven’t completed their core education—secondary school—have few or no marketable skills, are financially dependent upon her parents and/or continue to live at home. Often they are no mentally mature enough to raise a child on their own, which leads to problems for the mother as well as for the child.
-        </div>
-        
-        Causes and Effects of Teen Pregnancy
+        return (
+            <div class="Intro">
+                <div class="container">
+                    <div class="center">
+                        <div class="card sticky-action col s12">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <p></p>
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">
+                                    Teenage Pregnancy in the USA and factors influencing its occurence
+                                    <i class="material-icons right">more_vert</i></span>
+                                <p>
+                                    We want to have a look on pregnant teenage girls of the age of 15 to 19 
+                                    whereby those normally haven’t completed their core education—secondary 
+                                    school—have few or no marketable skills, are financially dependent upon 
+                                    her parents and/or continue to live at home. Often they are no mentally 
+                                    mature enough to raise a child on their own, which leads to problems for 
+                                    the mother as well as for the child. In the following the 3 
+                                    
+                                </p>
+                            </div>
+                            <div class="card-action">
+                                <a href="#" onClick={() => this.loadState('01')}>Educational Problems</a>
+                                <a href="#" onClick={() => this.loadState('11')}>Health issues</a>
+                                <a href="#" onClick={() => this.loadState('39')}>Direct financially</a>
+                                <a href="#" onClick={() => this.loadState('39')}>Socially</a>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">Teenage Pregnancy in the USA and factors influencing its occurence
+                                    <i class="material-icons right">close</i></span>
+                                <p>
+                                    Sources...
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="section no-pad-bot">
+                        <h3 class="center">Effects of Teen Pregnancy</h3>
+                    </div>
 
-          Teenage pregnancy is pregnancy in females under the age of 20. Additional concerns to the same pregnancy related issues from other womans face, teenagers below the age of 15 are very likely to be less developed enough to sustain a healthy pregnancy or to give birth. 
+                    <div class="row center">
+                        <div class="card sticky-action col s3">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <p></p>
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">
+                                    Educational Issues
+                                    <i class="material-icons right">more_vert</i></span>
+                                <p>
+                                  only 40% finish high school degree
+                                  college completion reduced by 65%
+                                </p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">Educational problems
+                                    <i class="material-icons right">close</i></span>
+                                <p>
+                                    
+                                </p>
+                            </div>
+                        </div>
 
-          In developed countries teenagers aged between 15 and 19 risks are more associated with socioeconomic issues than with the biological effects of age for both mother and child.
-          Educational issues:
+                        <div class="card sticky-action col s3">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <p></p>
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">
+                                    Health issues
+                                    <i class="material-icons right">more_vert</i></span>
+                                <p>
+                                    high risk of anaemia & blood pressure
+                                    premature birth and depression
+                                </p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">Abstinence-Only vs Comprehensive
+                                    <i class="material-icons right">close</i></span>
+                                <p>
+                                    Sources...
+                                </p>
+                            </div>
+                        </div>
 
-          Due to becoming pregnant during adolescence, teen mothers are highly likely to drop out of school. 
+                        <div class="card sticky-action col s3">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <p></p>
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">
+                                    Direct financial
+                                    <i class="material-icons right">more_vert</i></span>
+                                <p>
+                                    medical care for mother & child
+                                    child care and baby accessories
+                                </p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">Abstinence-Only vs Comprehensive
+                                    <i class="material-icons right">close</i></span>
+                                <p>
+                                    Sources...
+                                </p>
+                            </div>
+                        </div>
 
-          According to a nscl report only roughly 40 percent of teen mothers finish their high school degree. Fewer than 2 percent finish college by age 30. 
-          Young woman who give birth while attending community college are 65 percent less likely to complete their degree than woman who do not have children during that time.
-          Children of teen mothers perform worse on many measures of school readiness, are 50 percent more likely to repeat a grade, and are more likely than children born to older mothers to drop out of high school.
-          http://www.ncsl.org/research/health/teen-pregnancy-affects-graduation-rates-postcard.aspx
-          In the light of this, these young girls to not have full qualifications for proper jobs in the future, which leads to having a job with very low wages or even worse, unemployment. Overall, they are forced to delay and postpone any plans for their future in order to raise their child.
-          Health issues:
-          Pregnant teens often have heightened health risks, which inhibits healthy child development, suffering from such things as anaemia or blood pressure are possible during pregnancy. Other Health risks associated with teenage pregnancy are poor nutrition, premature birth and depression.
-          http://www.momjunction.com/articles/health-risks-of-teenage-pregnancy_00377831/#gref
-
-          Direct financial issues:
-          Teenage parents face a lot of problems from parenting to finances, especially because the teenagers hasn’t even started earning when the baby is born. In the states teenage mothers face a lot of issues when it comes to financially supporting medical care. If the mother has insurance only her medical problems are covered because the baby would need a seperate insurance. Child care and purchasing of baby accessories might not seem like a problem initially, but it adds up sooner or later. Accessories include wet wipes, diapers, ointments, bottles, etc. It is required to at least be able to take care of minimum wage, which is obviously difficult for a mother, who does little or no work to manage. 
-          Being reliant on their family for financial resources as well as support to help get her raising a child being exposed of the chance to be shunned and not accepted, because of the pregnancy.
-          http://www.onlymyhealth.com/financial-problems-teenage-pregnancy-1345312849
-          Social issues:
-          Having a child during these essential years truly clashes with the developmental tasks that should be occurring during adolescents. These young mothers are not able to fully develop a sense of self-identity because of their role as an expecting mother. Further, peer and social relationships are strained or even terminated and teen years are essentially for developing relationships with others and discovering oneself. 
-          https://books.google.ca/books?id=oQgvAAAAQBAJ&dq=parent+child+relations+3rd+edition&hl=en&sa=X&ei=85bZVMO5EIbnoATGqIKgCQ&ved=0CDEQ6AEwAA
-          Resulting in a higher chance to develop depression and other mental issues, being alienated from family or friends.
-          https://login.ezproxy.library.ubc.ca/login?url=http://knowledge.sagepub.com/view/familyhealth/n327.xml
-
-          Trends accounting for socioeconomic status, teen educational attainment, ethnic composition of the teen population.  
-
-        </div>
-      </div>
-    );
+                        <div class="card sticky-action col s3">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <p></p>
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">
+                                    Social Issues
+                                    <i class="material-icons right">more_vert</i></span>
+                                <p>
+                                  self-identity develop suffers
+                                  strained relationships with parents & friends
+                                </p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">Abstinence-Only vs Comprehensive
+                                    <i class="material-icons right">close</i></span>
+                                <p>
+                                    Sources...
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
   }
 }
