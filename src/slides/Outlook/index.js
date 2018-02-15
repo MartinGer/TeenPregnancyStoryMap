19513a403a732bf433835dedf5ec720db13f7b07
MartinGer <Gerstmaier.Martin@gmx.net>
2018-02-15 19:39:10 +0100

    fix bug

--- src/slides/Outlook/index.js
+++ src/slides/Outlook/index.js
@@ -14,15 +14,17 @@ export default class Outlook extends Component {
                         <div class="row center">
                             <div class="section col s5 push-s1">
                             <div class="card">
+                            <div class="cardOutlook">
                                 <div class="card-content">
                                     <span class="card-title activator grey-text text-darken-4">
                                        <h4> How will the Teenage Pregnancy rate develop in the future? </h4>
                                         </span>
-                                    <p>
+                                    <h5>
                                     The Trump administration has cut nearly $214 million from teen pregnancy prevention programs across the country.
 
                                     More than 80 organizations will lose Obama-era grants from the US Department of Health and Human Services
-                                    </p>
+                                    </h5>
+                                </div>
                                 </div>
                                 </div>
                         </div>
