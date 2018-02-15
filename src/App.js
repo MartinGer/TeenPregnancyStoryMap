3dc41e200c39d9db9ec13b8557dda23369a144f5
Sven Lehmann <sven.lehmann93@gmail.com>
2018-02-15 09:36:41 +0100

    Trying to get it running

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TeenPregnancyWorldwide from './slides/TeenPregnancyWorldwide'
 import TeenPregnancyUSStates from './slides/TeenPregnancyUSStates'
--- src/slides/TeenPregnancyWorldwide/index.js
+++ src/slides/TeenPregnancyWorldwide/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import Row from "antd/es/grid/row";
+import Col from "antd/es/grid/col";
 
-export default class TeenPregnancyUSStates extends Component {
+export default class TeenPregnancyWorldwide extends Component {
   constructor(props) {
     super(props);
 
@@ -8,10 +11,20 @@ export default class TeenPregnancyUSStates extends Component {
   }
 
   render() {
+    const mapCenter = [39.9528, -75.1638];
+    const zoomLevel = 15;
+
     return (
       <div>
-        TeenPregnancyUSStates
+        <Row type ='flex' gutter={16}>
+          <Map center={mapCenter} zoom={zoomLevel}>
+            <TileLayer
+              attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </Map>
+        </Row>
       </div>
-    );
+    )
   }
 }
