371ff05792cf033b63b6a4be994f62e3fe2f1942
Sven Lehmann <sven.lehmann93@gmail.com>
2018-02-15 10:08:42 +0100

    Minimal working map

--- src/slides/TeenPregnancyWorldwide/index.js
+++ src/slides/TeenPregnancyWorldwide/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class TeenPregnancyUSStates extends Component {
+export default class TeenPregnancyUSWorldwide extends Component {
   constructor(props) {
     super(props);
 
@@ -10,7 +10,7 @@ export default class TeenPregnancyUSStates extends Component {
   render() {
     return (
       <div>
-        TeenPregnancyUSStates
+        TeenPregnancyUSWorldwide
       </div>
     );
   }
