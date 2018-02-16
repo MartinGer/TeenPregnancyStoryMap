8af0260f76efc8ae9ce5fc1b1a58528febe7d0f8
Sven Lehmann <sven.lehmann93@gmail.com>
2018-02-16 18:35:03 +0100

    Adding working religion map

--- src/slides/ReligionUSStates/index.js
+++ src/slides/ReligionUSStates/index.js
@@ -1,49 +1,92 @@
 import React, {Component} from 'react';
 import './index.css'
+import {getStateName} from "../Util/fips";
+import {Chart} from 'react-google-charts';
 
 export default class ReligionUSStates extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        let options = {
+            region: 'US',
+            resolution: 'provinces',
+            colorAxis: {colors: ['#E8EAF6', '#0D47A1']},
+            chartArea: {width: '94%'},
+            width: '100%'
+        };
+
+        this.state = {options: options, data: []};
+    }
+
+    componentWillMount() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        fetch('http://vm-tuk2-team03.eaalab.hpi.uni-potsdam.de/api/usa/religion')
+            .then(results => {
+                console.log(results);
+                return results.json()
+            }).then(json => {
+                let usStates = [['State', 'Adherents in %']];
+
+                for (let i in json) {
+                    let state = json[i];
+                    usStates.push([
+                        state['state'],
+                        state['adherents %']
+                    ])
+                }
+
+                this.setState({data: usStates});
+            }
+        )
     }
 
     render() {
         return (
-            <div class="religion">
-                <div class="container section
-                 row">
-                    <div class="card col s8">
-                    </div>
-                    <div class="card col s4">
-                        <div class="card-image">
-                            <img src={require('./teen_pregnancy_2010.png')}/>
-                        </div>
-                        <div class="card-content">
-                            <span class="card-title activator grey-text text-darken-4">
-                                The impact of religion
-                                 <i class="material-icons right">more_vert</i>
-                            </span>
 
-                            <p class="section">
-                                Researchers, such as Joseph Strayhorn from the University of Pittsburgh believe to
-                                have found a link between teenage birth rates and strongly religious people. The
-                                teen pregnancy rate is actually higher among those communities. Possible explanations a
-                                re the <b>discouragement of abortions</b> or the use of <b>contraception</b>. But
-                                religion does not seem to solve our 2007 incident.
-                            </p>
-                        </div>
-
-                        <div class="card-reveal">
-                            <span class="card-title activator grey-text text-darken-4">
-                                The impact of religion
-                                <i class="material-icons right">close</i>
-                            </span>
-                            <p>TODO Add sources</p>
-                        </div>
-                    </div>
+          <div class="container section center row">
+              <div class="section">
+                  <h4>Looking at Adherent Rates</h4>
+              </div>
+              <div class="container col s8">
+                <Chart
+                    graph_id="ReligionChart"
+                    chartType="GeoChart"
+                    data={this.state.data}
+                    options={this.state.options}
+                    legend_toggle={true}
+                    width={"100%"}
+                    height={"100%"}
+                />
+              </div>
+              <div class="card sticky-action col s4">
+                <div class="card-image">
+                  <img src={require('./teen_pregnancy_2010.png')}/>
                 </div>
-            </div>
+                  <div class="card-content">
+                      <span class="card-title activator grey-text text-darken-4">
+                          The impact of religion (2010)
+                          <i class="material-icons right">more_vert</i>
+                      </span>
+                      <p class="section">
+                          Researchers, such as Joseph Strayhorn from the University of Pittsburgh, believe to
+                          have found a link between teenage birth rates and strongly religious people. The
+                          teen pregnancy rate is actually higher among those communities. Possible explanations are
+                          the <b>discouragement of abortions</b> or the use of <b>contraception</b>. But
+                          religion does not seem to solve our 2007 incident.
+                      </p>
+                  </div>
+                  <div class="card-reveal">
+                    <span class="card-title activator grey-text text-darken-4">
+                        The impact of religion
+                        <i class="material-icons right">close</i>
+                    </span>
+                    <p>TODO Add sources</p>
+                  </div>
+              </div>
+          </div>
         );
     }
 }
